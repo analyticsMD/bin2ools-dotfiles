78df5e4c87250d6785d4e6610008eda06974a7f8
Marc Anthony Slayton <4723322+gangofnuns@users.noreply.github.com>
2022-04-24 16:47:06 -0700

    DEVOPS-5650: removing clutter that cause netowrk outages.

---
+++ bt/.local/share/devops-sso-util/complete.sh
@@ -0,0 +1,15 @@
+Usage: devops-sso-util [OPTIONS] COMMAND [ARGS]...
+
+Options:
+  --version  Show the version and exit.
+  --help     Show this message and exit.
+
+Commands:
+  admin      Commands for AWS SSO administration.
+  check      Debug AWS SSO configuration and access.
+  configure  Commands to set up ~/.aws/config.
+  console    Commands for launching the AWS console in a browser.
+  login      Log in to an AWS SSO instance.
+  logout     Log out of all AWS SSO sessions
+  roles      List your available accounts and roles.
+  run-as     Run a command as a specific account + role.
