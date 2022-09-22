3342ccd8eea26db4134db98a0670c5d95e8fcf36
Marc Slayton <marc@qventus.com>
2022-09-22 09:09:50 -0700

    DEVOPS-5650: fixes

--- inst_lite.sh
+++ inst_lite.sh
@@ -4,30 +4,33 @@
 [ -z "${HOME}" ] && echo "no HOME var." && exit 1
 [[ "${SHELL##*/}" != "bash" ]] && echo "wrong SHELL." && exit 1
 [[ "${BASH_VERSION%%.*}" -lt 5 ]] && echo "bad shell version: ${BASH_VERSION}"
-
-rm -rf ${HOME}/.bt ${HOME}/bintools ${HOME}/.bin2ools-dotfiles ${HOME}/.brew
-
-
-# removed /usr/local/bin links to ~/.local and ~/.brew
+[[ ! -d "${HOME}/.bin2ools-dotfiles" ]] && echo "No bin2ools-dotfiles repo." && exit 1
+[[ ! -f "${HOME}/.bin2ools-dotfiles/bt/utils/path" ]] && echo "No bt path utility." && exit 1
+rm -rf ${HOME}/.bt ${HOME}/bintools ${HOME}/.brew
+#git clone git@github.com:analyticsmd/bin2ools-dotfiles.git ~/.bin2ools-dotfiles
+ln -fs ~/.bin2ools-dotfiles/bt ~/.bt 
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' > ~/.tmp_profile
-echo "$(/opt/homebrew/bin/brew shellenv | tee ${HOME}/.tmp_profile)"
-[ -f "${HOME}/.tmp_profile" ] && . ${HOME}/.tmp_profile
 
-brew update
-brew upgrade
+[[ ! -f "${HOME}/.bash_profile" ]] && { 
+  TMP_PROFILE="${HOME}/.tmp_profile.$$"
+  OLD_PROFILE="${HOME}/.bash_profile.$$"
+  echo "$(/opt/homebrew/bin/brew shellenv)" | tee "${TMP_PROFILE}"
+  ${HOME}/.bt/utils/path -s >> "${TMP_PROFILE}"
+  mv "${HOME}/.bash_profile" "${OLD_PROFILE}"
+  mv "${TMP_PROFILE}" ${HOME}/.bash_profile
+}
+
+#brew update
+#brew upgrade
 brew doctor
 
-git clone git@github.com:analyticsmd/bin2ools-dotfiles.git ~/.bin2ools-dotfiles
-ln -fs ~/.bin2ools-dotfiles/bt ~/.bt 
-
+echo installing Brewfile
 brew bundle --file ~/.bin2ools-dotfiles/Brewfile 
 
-export PATH="$PATH:/Users/${HOME##*/}/.local/bin"
-echo 'export PATH="$PATH:/Users/${HOME##*/}/.local/bin"' >> ${HOME}/.tmp_profile
-mv ${HOME}/.bash_profile ${HOME}/.bash_profile.bak.$$
-mv ${HOME}/.tmp_profile ${HOME}/.bash_profile
+#export PATH="$PATH:/Users/${HOME##*/}/.local/bin"
+#echo 'export PATH="$PATH:/Users/${HOME##*/}/.local/bin"' >> ${HOME}/.tmp_profile
 
 echo Running pipx installs...
 ${HOME}/.bt/inst/python-pipx
